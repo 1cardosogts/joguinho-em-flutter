4592a28e806e7ac8d73411888232e7e91df80560
Gustavo Cardoso <112995671+1cardosogts@users.noreply.github.com>
2023-10-06 16:00:36 -0300

    Update joguinho em flutter NIM.dart

--- joguinho em flutter NIM.dart
+++ joguinho em flutter NIM.dart
@@ -39,105 +39,101 @@ class _GameScreenState extends State<GameScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('GUSTAVO CARDOSO SANTOS DA SILVA 1431432312003'),
+        title: Text('GUSTAVO CARDOSO SANTOS DA SILVA, 1431432312003'),
       ),
-      body: Container(
-        color: Colors.black,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              if (!gameOver)
-                Column(
-                  children: [
-                    Text(
-                      'Palitos no jogo: $palitosFaltantes',
-                      style: TextStyle(color: Colors.white, fontSize: 18.0),
-                    ),
-                    Text(
-                      'Máximo a retirar: $maxRetirar',
-                      style: TextStyle(color: Colors.white, fontSize: 18.0),
-                    ),
-                    Text(
-                      playerTurn ? 'Sua vez!' : 'Vez do computador!',
-                      style: TextStyle(color: Colors.white, fontSize: 18.0),
-                    ),
-                    Container(
-                      width: 200.0,
-                      child: TextField(
-                        controller: controller,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          labelText: 'Quantidade de Palitos a Retirar',
-                          labelStyle: TextStyle(color: Colors.white),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                        ),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        final input = controller.text;
-                        if (input.isNotEmpty && input.replaceAll(RegExp(r'\d'), '') == '') {
-                          final int quantidadeRetirar = int.parse(input);
-                          if (quantidadeRetirar >= 1 &&
-                              quantidadeRetirar <= maxRetirar &&
-                              quantidadeRetirar <= palitosFaltantes) {
-                            setState(() {
-                              palitosRetirados += quantidadeRetirar;
-                              palitosFaltantes -= quantidadeRetirar;
-                              playerTurn = !playerTurn;
-                              if (palitosFaltantes == 0) {
-                                gameOver = true;
-                              } else {
-                                final int quantidadeComputador = palitosFaltantes % (maxRetirar + 1);
-                                palitosRetirados += quantidadeComputador;
-                                palitosFaltantes -= quantidadeComputador;
-                                playerTurn = true;
-                                if (palitosFaltantes == 0) {
-                                  gameOver = true;
-                                }
-                              }
-                            });
-                          }
-                        }
-                        controller.clear();
-                      },
-                      child: Text('Retirar'),
-                    ),
-                  ],
-                )
-              else
-                Column(
-                  children: [
-                    Text(
-                      'Fim do jogo! ${playerTurn ? "Você perdeu!" : "Você ganhou!"}',
-                      style: TextStyle(color: Colors.white, fontSize: 18.0),
-                    ),
-                    Text(
-                      'Palitos retirados: $palitosRetirados',
-                      style: TextStyle(color: Colors.white, fontSize: 18.0),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          startGame();
-                        });
-                      },
-                      child: Text('Reiniciar Jogo'),
-                    ),
-                  ],
-                ),
-            ],
-          ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (!gameOver)
+              Column(
+                children: [
+                  _texto('Palitos no jogo: $palitosFaltantes'),
+                  _texto('Máximo a retirar: $maxRetirar'),
+                  _texto(playerTurn ? 'Sua vez!' : 'Vez do computador!'),
+                  _input(),
+                  ElevatedButton(
+                    onPressed: () => _retirarPalitos(),
+                    child: Text('Retirar'),
+                  ),
+                ],
+              )
+            else
+              Column(
+                children: [
+                  _texto('Fim do jogo! ${playerTurn ? "Você perdeu!" : "Você ganhou!"}'),
+                  _texto('Palitos retirados: $palitosRetirados'),
+                  ElevatedButton(
+                    onPressed: () => _reiniciarJogo(),
+                    child: Text('Reiniciar Jogo'),
+                  ),
+                ],
+              ),
+          ],
         ),
       ),
     );
+  }
+
+  Widget _texto(String texto) {
+    return Text(
+      texto,
+      style: TextStyle(color: Colors.white, fontSize: 18.0),
+    );
+  }
+
+  Widget _input() {
+    return Container(
+      width: 200.0,
+      child: TextField(
+        controller: controller,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          labelText: 'Quantidade de Palitos a Retirar',
+          labelStyle: TextStyle(color: Colors.white),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+        ),
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  void _retirarPalitos() {
+    final input = controller.text;
+    if (input.isNotEmpty && input.replaceAll(RegExp(r'\d'), '') == '') {
+      final int quantidadeRetirar = int.parse(input);
+      if (quantidadeRetirar >= 1 && quantidadeRetirar <= maxRetirar && quantidadeRetirar <= palitosFaltantes) {
+        setState(() {
+          palitosRetirados += quantidadeRetirar;
+          palitosFaltantes -= quantidadeRetirar;
+          playerTurn = !playerTurn;
+          if (palitosFaltantes == 0) {
+            gameOver = true;
+          } else {
+            final int quantidadeComputador = palitosFaltantes % (maxRetirar + 1);
+            palitosRetirados += quantidadeComputador;
+            palitosFaltantes -= quantidadeComputador;
+            playerTurn = true;
+            if (palitosFaltantes == 0) {
+              gameOver = true;
+            }
+          }
+        });
+      }
+    }
+    controller.clear();
+  }
+
+  void _reiniciarJogo() {
+    setState(() {
+      startGame();
+    });
   }
 
   void startGame() {
